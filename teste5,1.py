30c4eb207bc0389ac0898d31ba4f74d203a421bf
Benjib5 <168489417+Benjib5@users.noreply.github.com>
2024-05-17 11:47:59 -0300

    Update teste5,1.py

--- teste5,1.py
+++ teste5,1.py
@@ -1,13 +1,13 @@
-import csv
 import streamlit as st
 import pandas as pd
 import numpy as np
+import csv
 
 chart_data = pd.DataFrame(
    {
-       "line1,2,3": np."Mago Negro",
-       "line4,5,6": np."Dragão Negro de Olhos Vermelhos",
+       "col1": np."Nome",
+       "col2": np."Preço",
    }
 )
 
-st.line_chart(chart_data, x="col1", y="col5")
+st.line_chart(chart_data, x="col1", y="col2")
