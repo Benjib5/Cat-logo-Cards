67e950caf34fa57641fabb022ac161e4146073b4
Benjib5 <168489417+Benjib5@users.noreply.github.com>
2024-05-17 11:39:36 -0300

    Update teste5,1.py

--- teste5,1.py
+++ teste5,1.py
@@ -1,25 +1,13 @@
-import pandas as pd
-import streamlit as st
 import csv
-from matplotlib import pyplot as plt
+import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title("Card Price")
+chart_data = pd.DataFrame(
+   {
+       "col1": np."Nome",
+       "col5": np."Preço,
+   }
+)
 
-stw = pd.read_csv("Pasta de Yugioh - Página1.csv")
-
-stw_chart = plt.gca()
-st.line_chart(stw_chart,str, x=stw.Height, y=stw.Height, width=500, height=500, use_container_width=True)
-
-
-contact_options = ["Height","Weight"]
-contact_selected = st.selectbox("Select a Students value",
-                               options = contact_options)
-
-
-if contact_selected =="Height":
-    plt.plot(stw.Name, stw.Height)
-    plt.show()
-    st.write("result of Height")
-else:
-    plt.plot(stw.Name, stw.Weight)
-    plt.show()
+st.line_chart(chart_data, x="col1", y="col5")
