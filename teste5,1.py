507f7a9801726c444fceb7247392ea3665058dab
Benjib5 <168489417+Benjib5@users.noreply.github.com>
2024-05-17 11:51:18 -0300

    Update teste5,1.py

--- teste5,1.py
+++ teste5,1.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import csv
 
-chart_data = pd.DataFrame(np.columns=["col1", "col5"])
+chart_data = pd.DataFrame(columns=["col1", "col5"])
 
 st.line_chart(
    chart_data, x="col1", y=["col2"], color=["#FF0000", "#0000FF"]
