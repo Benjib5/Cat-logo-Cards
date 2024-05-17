802ab1b28fc21aef438f2bd3a334a854de6bf7a9
Benjib5 <168489417+Benjib5@users.noreply.github.com>
2024-05-17 11:59:58 -0300

    Update teste5.py

--- teste5.py
+++ teste5.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pandas
-arquivo_excel = pandas.read_excel('Pasta de Yugioh.xlsx')
-arquivo_excel
+import csv
 
-chart_data = pd.DataFrame(
-   {
-       "col1": np.random.randn(20),
-       "col2": np.random.randn(20),
-       "col3": np.random.choice(["A", "B", "C"], 20),
-   }
+chart_data = pd.DataFrame(np.columns=["col1", "col2", "col3"])
+      col1 = Pasta de Yugioh - Página1.csv['Raridade']
+      col2 = Pasta de Yugioh - Página1.csv['Preço']
+chart_data['col4'] = np.(['Magro Negro','Dragão Negro de Olhos Vermelhos','Exodia,"O Proibido"'])
+
+st.scatter_chart(
+    chart_data,
+    x='col1',
+    y='col2',
+    card='col4',
+    size='col3',
 )
-
-st.line_chart(chart_data, x="col1", y="col2", color="col3")
