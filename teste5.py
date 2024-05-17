e4fdf331de5b5b45adb0409231776fdf60967014
Benjib5 <168489417+Benjib5@users.noreply.github.com>
2024-05-17 12:10:06 -0300

    Update teste5.py

--- teste5.py
+++ teste5.py
@@ -1,16 +1,13 @@
 import streamlit as st
 import pandas as pd
 
+# Carregar os dados do arquivo CSV
 file_path = "Pasta de Yugioh - Página1.csv"
 data = pd.read_csv(file_path)
 
-data.columns = ["Raridade", "Preço"]
-
-data["Carta"] = ['Magro Negro', 'Dragão Negro de Olhos Vermelhos', 'Exodia, "O Proibido"']
-
+# Exibir gráfico de dispersão
 st.scatter_chart(
     data=data,
     x='Raridade',
     y='Preço',
     size='Carta'
-)
